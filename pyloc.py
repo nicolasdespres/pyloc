964682970a920058038575f2f196818ff7900e99
Nicolas Despres <nicolas.despres@gmail.com>
2015-04-27 00:32:49 +0200

    Also provide line number.

--- pyloc.py
+++ pyloc.py
@@ -1,73 +1,191 @@
 # -*- encoding: utf-8 -*-
 """Prints the location of the definition a any python object in your file-system.
-
-Copyright (c) 2015, Nicolas Despres
 """
 
 
 from __future__ import print_function
 import sys
 import argparse
-from importlib import import_module
+import importlib
+import inspect
+import re
 import os
+from textwrap import dedent
 
 
-def subnames(name):
-    curname = ""
-    names = []
-    for n in name.split("."):
-        if curname:
-            curname += "." + n
+class PylocError(Exception):
+    """Base class of all exception raised by this module."""
+
+class ModuleNameError(PylocError):
+
+    def __init__(self, name, error):
+        self.name = name
+        self.error = error
+
+    def __str__(self):
+        return "failed to import '{}' ({}: {})"\
+            .format(self.name,
+                    type(self.error).__name__,
+                    self.error)
+
+class AttributeNameError(PylocError):
+
+    def __init__(self, prefix, name):
+        self.prefix = prefix
+        self.name = name
+
+    def __str__(self):
+        return "cannot get attribute '%s' from '%s'" %(self.name, self.prefix)
+
+def pyloc(target):
+    """Return (filename, lineno) defining object named "module[:qualname]".
+
+    `lineno` may be None if not applicable (i.e. for module or package) or
+    if cannot be found.
+
+    Inspired by 'inspect._main()' and 'inspect.findsource()' by
+      Ka-Ping Yee <ping@lfw.org> and
+      Yury Selivanov <yselivanov@sprymix.com>
+    """
+    if not target:
+        raise ValueError("target must be a non-empty string")
+    mod_name, has_attrs_name, attrs_name = target.partition(":")
+    ### Try to import the module containing the given target.
+    try:
+        obj = module = importlib.import_module(mod_name)
+    except Exception as exc:
+        raise ModuleNameError(mod_name, exc)
+    ### Get the object in module
+    if has_attrs_name:
+        attrs = attrs_name.split(".")
+        for i in range(len(attrs)):
+            attr = attrs[i]
+            try:
+                new_obj = getattr(obj, attr)
+            except AttributeError:
+                raise AttributeNameError(".".join([module.__name__]+attrs[:i]),
+                                         attr)
+            else:
+                # If new_obj is not a class, method or function, we won't be
+                # able to get the file defining it so we stick stop at the
+                # previous object. It may happens when target is a constant.
+                if inspect.isclass(new_obj) \
+                   or inspect.ismethod(new_obj) \
+                   or inspect.isfunction(new_obj):
+                    obj = new_obj
+                else:
+                    break
+    ### Get location
+    filename = inspect.getsourcefile(obj)
+    if not filename:
+        return inspect.getfile(obj), None
+    if inspect.ismodule(obj):
+        return filename, None
+    if inspect.isclass(obj):
+        name = obj.__name__
+        pat = re.compile(r'^(\s*)class\s*' + name + r'\b')
+        # make some effort to find the best matching class definition:
+        # use the one with the least indentation, which is the one
+        # that's most probably not inside a function definition.
+        candidates = []
+        with open(filename) as f:
+            for i, line in enumerate(f):
+                lineno = i + 1
+                match = pat.match(line)
+                if match:
+                    # if it's at toplevel, it's already the best one
+                    if line[0] == 'c':
+                        return filename, lineno
+                    # else add whitespace to candidate list
+                    candidates.append((match.group(1), lineno))
+        if candidates:
+            # this will sort by whitespace, and by line number,
+            # less whitespace first
+            candidates.sort()
+            return filename, candidates[0][1]
         else:
-            curname = n
-        names.append(curname)
-    names.reverse()
-    return names
+            return filename, None
+    if inspect.ismethod(obj):
+        obj = obj.__func__
+    if inspect.isfunction(obj):
+        obj = obj.__code__
+    if inspect.istraceback(obj):
+        obj = obj.tb_frame
+    if inspect.isframe(obj):
+        obj = obj.f_code
+    if inspect.iscode(obj):
+        if not hasattr(obj, 'co_firstlineno'):
+            return filename, None
+        return filename, obj.co_firstlineno
+    return filename, None
 
-def raw_pyfile(name):
-    first_import_err = None
-    for n in subnames(name):
-        try:
-            mod = import_module(n)
-        except ImportError as e:
-            if first_import_err is None:
-                first_import_err = e
-        else:
-            return mod.__file__
-    raise e
+# =============================== #
+# Command line interface function #
+# =============================== #
 
-def pyc2py(pathname):
-    assert pathname.endswith(".pyc")
-    return pathname[:-1]
+DEFAULT_LOC_FORMAT = "emacs"
 
-def pyfile(name):
-    path = raw_pyfile(name)
-    if path.endswith(".pyc"):
-        py_path = pyc2py(path)
-        if os.path.exists(py_path):
-            return py_path
-    return path
+def format_loc(filename, lineno, format=DEFAULT_LOC_FORMAT):
+    if format == 'emacs':
+        s = ""
+        if lineno:
+            s += "+%d " %(lineno,)
+        s += filename
+        return s
+    elif format == 'vi':
+        s = filename
+        if lineno:
+            s += ":%d" %(lineno,)
+        return s
+    else:
+        raise ValueError("unsupported format: {}".format(format))
+
+_EPILOGUE = """
+environment variables:
+ PYLOC_DEFAULT_FORMAT - default output format (default: {default_format})
+
+Copyright (c) 2015, Nicolas Despres
+All right reserved.
+""".format(
+    default_format=DEFAULT_LOC_FORMAT,
+    )
 
 def build_cli():
+    class RawDescriptionWithArgumentDefaultsHelpFormatter(
+            argparse.ArgumentDefaultsHelpFormatter,
+            argparse.RawDescriptionHelpFormatter,
+    ):
+        """Mix both formatter."""
     parser = argparse.ArgumentParser(
         description=__doc__,
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        epilog=dedent(_EPILOGUE),
+        formatter_class=RawDescriptionWithArgumentDefaultsHelpFormatter)
     parser.add_argument(
-        "name",
+        "-f", "--format",
         action="store",
-        help="A python qualified name.")
+        choices=("emacs", "vi"),
+        default=os.environ.get("PYLOC_DEFAULT_FORMAT", DEFAULT_LOC_FORMAT),
+        help="How to write object location")
+    parser.add_argument(
+        "object_name",
+        action="store",
+        help="A python object named: module[:qualname]")
     return parser
 
 def main(argv):
     cli = build_cli()
     options = cli.parse_args(argv[1:])
-    filename = pyfile(options.name)
-    if filename:
-        print(filename)
-        return 0
-    else:
-        print("pyfile: cannot find definition file for '%s'" % options.name)
+    try:
+        filename, lineno = pyloc(options.object_name)
+    except PylocError as e:
+        sys.stderr.write("pyloc: ")
+        sys.stderr.write(str(e))
+        sys.stderr.write("\n")
         return 1
+    else:
+        sys.stdout.write(format_loc(filename, lineno, format=options.format))
+        sys.stdout.write("\n")
+        return 0
 
 if __name__ == "__main__":
     sys.exit(main(sys.argv))
---
+++ test_pyloc.py
@@ -0,0 +1,134 @@
+# -*- encoding: utf-8 -*-
+"""Test 'pyloc' module.
+"""
+
+
+import unittest
+import sysconfig
+import sys
+import os
+import contextlib
+import re
+
+from pyloc import pyloc
+from pyloc import ModuleNameError
+from pyloc import AttributeNameError
+
+
+# Guidelines:
+# - Use only fixture names that are part of the standard library for all
+#   supported version of Python and that are not imported by either this
+#   module and pyloc.
+
+
+PLATSTDLIB_PATH = sysconfig.get_path('platstdlib')
+
+@contextlib.contextmanager
+def save_sys_modules():
+    saved_modules = sys.modules.copy()
+    try:
+        yield
+    finally:
+        to_del = []
+        for k in sys.modules:
+            if k not in saved_modules:
+                to_del.append(k)
+        for k in to_del:
+            del sys.modules[k]
+
+def get_line(filename, lineno):
+    with open(filename) as f:
+        for i, line in enumerate(f):
+            if i + 1 == lineno:
+                return line
+
+class TestPyloc(unittest.TestCase):
+
+    # Duplicated from unittest.TestCase because it is named assertRegexpMatches
+    # in python 2.7.x
+    def assertRegexp(self, text, expected_regex, msg=None):
+        if isinstance(expected_regex, (str, bytes)):
+            assert expected_regex, "expected_regex must not be empty."
+            expected_regex = re.compile(expected_regex)
+        if not expected_regex.search(text):
+            msg = msg or "Regex didn't match"
+            msg = '%s: %r not found in %r' % (msg, expected_regex.pattern, text)
+            raise self.failureException(msg)
+
+    def assertLocEqual(self, expected, modname, qualname=None,
+                       match_line=None):
+        self.assertFalse(modname in sys.modules,
+                         "'%s' is already imported" % (modname,))
+        fullname = modname
+        if qualname:
+            fullname += ":" + qualname
+        if not os.path.isabs(expected):
+            expected = os.path.join(PLATSTDLIB_PATH, expected)
+        with save_sys_modules():
+            filename, lineno = pyloc(fullname)
+        if match_line is not None:
+            self.assertIsInstance(lineno, int)
+            self.assertIsNotNone(lineno, filename)
+            line = get_line(filename, lineno)
+            self.assertRegexp(line,
+                              r"^\s*{}\s+[_a-zA-Z]+".format(match_line))
+        self.assertEqual(expected, filename)
+
+    def test_module_name_error(self):
+        with self.assertRaises(ModuleNameError):
+            pyloc("list")
+
+    def test_attribute_name_error(self):
+        with self.assertRaises(AttributeNameError) as cm:
+            with save_sys_modules():
+                pyloc("subprocess:doesnotexist")
+        self.assertRegexp(str(cm.exception),
+                          r"'doesnotexist'.+'subprocess'")
+
+    def test_sub_attribute_name_error(self):
+        with self.assertRaises(AttributeNameError) as cm:
+            with save_sys_modules():
+                pyloc("subprocess:Popen.doesnotexist")
+        self.assertRegexp(str(cm.exception),
+                          r"'doesnotexist'.+'subprocess.Popen'")
+
+
+    def test_package(self):
+        self.assertLocEqual("email/__init__.py", "email")
+
+    def test_no_source_file(self):
+        filename = os.path.join(sysconfig.get_config_var("DESTSHARED"),
+                                "zlib.so")
+        self.assertLocEqual(filename, "zlib")
+
+    def test_module(self):
+        self.assertLocEqual("subprocess.py", "subprocess")
+
+    def test_module_in_package(self):
+        self.assertLocEqual("email/utils.py", "email.utils")
+
+    def test_function_in_module_in_package(self):
+        self.assertLocEqual("email/utils.py", "email.utils",
+                            qualname="formataddr",
+                            match_line='def')
+
+    def test_function_in_module(self):
+        self.assertLocEqual("subprocess.py", "subprocess",
+                            qualname="check_output",
+                            match_line='def')
+
+    def test_class(self):
+        self.assertLocEqual("subprocess.py", "subprocess",
+                            qualname="Popen",
+                            match_line='class')
+
+    def test_method(self):
+        self.assertLocEqual("subprocess.py", "subprocess",
+                            qualname="Popen.wait",
+                            match_line='def')
+
+    def test_constant(self):
+        self.assertLocEqual("subprocess.py", "subprocess", qualname="PIPE")
+
+    def test_unicode(self):
+        self.assertLocEqual(u"subprocess.py", u"subprocess", qualname=u"PIPE")

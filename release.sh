909e80692f85e88a0c8a6aad29a7753ceee6991a
Nicolas Despres <nicolas.despres@gmail.com>
2015-06-23 00:14:56 +0200

    Add option to control release process.
    release.sh is now in charge of tagging the repository and to push the
    refspec of the tag and the branch to the remote repository. It is also
    possible to disable part of the release procedure while debugging.

--- release.sh
+++ release.sh
@@ -3,11 +3,16 @@
 #::BEGIN::
 # USAGE
 #
-#  release.sh
+#  release.sh [options] <version>
 #
 # DESCRIPTION
 #
-#  Release this project on pypi.
+#  Tag the given <version>, create a distribution tarball, check it and
+#  release it on pypi.
+#
+# OPTIONS
+#
+#
 #
 # Copyright (c) 2015, Nicolas Despres
 # Report any problem to <nicolas.despres@gmail.com>
@@ -21,7 +26,23 @@ set -o nounset
 export LC_ALL=C
 unset CDPATH
 
+# ======================= #
+# Configuration variables #
+# ======================= #
+
 : ${RELEASE_DEBUG:=false}
+
+# ========= #
+# Functions #
+# ========= #
+
+# Print the message in the header of this file.
+usage()
+{
+  sed -ne '/^#::BEGIN::/,/^#::END::/p' < "$0" \
+    | sed -e '/^#::BEGIN::/d;/^#::END::/d' \
+    | sed -e 's/^# //; s/^#//'
+}
 
 # Print its arguments on stderr prefixed by the base name of this script and
 # a 'fatal' tag.
@@ -45,6 +66,11 @@ cleanup()
      "VERSION.txt"
 }
 
+remove_tag()
+{
+  git tag -d v$VERSION 2>/dev/null || true
+}
+
 # Called when the script exit.
 on_exit()
 {
@@ -55,6 +81,7 @@ on_exit()
 on_interrupt()
 {
   echo "Release script has been interrupted!!!"
+  remove_tag
   exit 2
 }
 
@@ -63,6 +90,51 @@ is_absolute()
 {
   test -z "${1##/*}"
 }
+
+check_version_format()
+{
+  grep -q -E '^[0-9]+\.[0-9]+\.[0-9]+$'
+}
+
+# ========================== #
+# Parse command line options #
+# ========================== #
+
+if [ $# -lt 1 ]
+then
+  usage
+  exit 1
+fi
+
+NO_PUSH=false
+NO_UPLOAD=false
+NO_TEST=false
+NO_DISTCHECK=false
+PYPI_REPO=pypi
+TAG_MSG_FILE=
+while [ $# -gt 0 ]
+do
+  arg="$1"
+  case "$arg" in
+    --no-push) NO_PUSH=true;;
+    --no-upload) NO_UPLOAD=true;;
+    --no-test) NO_TEST=true;;
+    --no-distcheck) NO_DISTCHECK=true;;
+    --repo=*) PYPI_REPO=$(sed -e 's/^--repo=//' <<< "$arg");;
+    --tag-msg=*) TAG_MSG_FILE=$(sed -e 's/^--tag-msg=//' <<< "$arg");;
+    -h|--help) usage; exit 1;;
+    -*) fatal "unknown option '$arg'";;
+    *) break;;
+  esac
+  shift
+done
+
+VERSION="$1"
+check_version_format <<< "$VERSION" || fatal "invalid version format '$VERSION'"
+
+# ======================= #
+# Main script entry point #
+# ======================= #
 
 ### Move to repository root directory
 if is_absolute "$0"
@@ -82,10 +154,10 @@ CURRENT_BRANCH=$(git branch --no-color | sed -ne '/^\* */s///p')
 test "$CURRENT_BRANCH" = "master" || fatal "not on master branch"
 
 ### Run test
-for version in 2.7.10 3.2.3 3.2.6 3.3.6 3.4.3
-do
-  PYENV_VERSION=$version python -m unittest test_pyloc
-done
+if ! $NO_TEST
+then
+  ./runtest.sh 2>&1 | sed -e 's/^/runtest.sh: /'
+fi
 
 ### Cleanup release/build directories
 trap -- on_exit EXIT
@@ -93,18 +165,37 @@ trap -- on_interrupt INT
 cleanup
 rm -rf dist build pyloc.egg-info
 
+### Tag repository
+remove_tag
+GIT_TAG_ARGS="-a"
+test -n "$TAG_MSG_FILE" && GIT_TAG_ARGS="$GIT_TAG_ARGS -F $TAG_MSG_FILE"
+git tag $GIT_TAG_ARGS v$VERSION master
+
 ### Generate and check version
-VERSION=$(git describe --dirty --always --match 'v*' | sed -e 's/^v//')
-echo "$VERSION" > VERSION.txt
-grep -q -E '^[0-9]+\.[0-9]+\.[0-9]+$' VERSION.txt \
-  || fatal "invalid version '$VERSION'"
+GIT_VERSION=$(git describe --dirty --always --match 'v*' | sed -e 's/^v//')
+echo "$GIT_VERSION" > VERSION.txt
+check_version_format <<< "$GIT_VERSION" \
+  || fatal "invalid version '$GIT_VERSION'"
 
 ### Build distribution
 python setup.py sdist
 python setup.py bdist_wheel --universal
 
 ### Test distribution
-./distcheck.sh "$ME_DIR/dist/pyloc-$VERSION.tar.gz"
+if ! $NO_DISTCHECK
+then
+  ./distcheck.sh "$ME_DIR/dist/pyloc-$VERSION.tar.gz" 2>&1 \
+    | sed -e 's/^/distcheck.sh: /'
+fi
+
+### Push
+if ! $NO_PUSH
+then
+  git push --follow-tags origin master
+fi
 
 ### Upload
-twine upload dist/*
+if ! $NO_UPLOAD
+then
+  twine upload -r $PYPI_REPO dist/*
+fi
---
+++ runtest.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+#::BEGIN::
+# USAGE
+#
+#  runtest.sh
+#
+# DESCRIPTION
+#
+#  Run test suite against several version of python. This script is called
+#  by the release script.
+#
+# Copyright (c) 2015, Nicolas Despres
+# Report any problem to <nicolas.despres@gmail.com>
+#::END::
+#
+
+set -o errexit
+set -o nounset
+
+export LC_ALL=C
+unset CDPATH
+
+
+# ======================= #
+# Script main entry point #
+# ======================= #
+
+for version in 2.7.10 3.2.3 3.2.6 3.3.6 3.4.3
+do
+  PYENV_VERSION=$version python -m unittest test_pyloc
+done
